bd5aff8d3d79a003e83a7ba4f397a07dd5493f08
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-20 17:50:39 +0530

    custom input feature added

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ let arr = [[], [], [], [], [], [], [], [], []]
 for (let i = 0; i < 9; i++) {
 	for (let j = 0; j < 9; j++) {
 		arr[i][j] = document.getElementById(i * 9 + j);
-
+		// console.log(typeof arr[i][j]); //object
 	}
 }
+//  minimum 17 input is needed so we have to count to check for input 
+let cnt = 0;
 
-
-let board = [[], [], [], [], [], [], [], [], []]
+let board = [[], [], [], [], [], [], [], [], []];
+for(let i=0; i<9; i++){
+	for(let j=0; j<9; j++){
+		board[i][j] = 0;
+	}
+}
 
 function FillBoard(board) {
 	for (let i = 0; i < 9; i++) {
@@ -23,11 +29,27 @@ function FillBoard(board) {
 	}
 }
 
+// custom input me board ko unfill kro
+function UnFillBoard() {
+	for (let i = 0; i < 9; i++) {
+		for (let j = 0; j < 9; j++) {
+
+			arr[i][j].innerText = '';
+			board[i][j] = 0;
+		}
+	}
+}
+
 let GetPuzzle = document.getElementById('GetPuzzle')
 let SolvePuzzle = document.getElementById('SolvePuzzle')
 
 
 GetPuzzle.onclick = function () {
+	cnt = 17;
+	const editableDivs = document.querySelectorAll(".clickable");
+	editableDivs.forEach((element) => {
+		element.setAttribute("contentEditable", "false");
+	});
 	let xhrRequest = new XMLHttpRequest()
 	xhrRequest.onload = function () {
 		let response = JSON.parse(xhrRequest.response)
@@ -42,17 +64,78 @@ GetPuzzle.onclick = function () {
 	xhrRequest.send()
 }
 
+
+const editableDivs = document.querySelectorAll(".clickable");
+function handleKeyDown(event) {
+	// Get the key code from the event
+	const keyCode = event.keyCode;
+
+	// Check if the pressed key is a number (0-9)
+	if (keyCode >= 48 && keyCode <= 57) {
+
+		// Find the currently focused div (the one that has focus)
+		const focusedDiv = document.activeElement;
+		focusedDiv.innerHTML = (keyCode - 49 + 1);
+		let number = focusedDiv.id;
+		let row = Math.floor(number / 9);
+		let col = number % 9;
+		// console.log(row, col);
+		board[row][col] = (keyCode - 49 + 1)
+		cnt++;
+	}
+	else if(keyCode >= 97 && keyCode <= 109)
+	{
+		const focusedDiv = document.activeElement;
+		focusedDiv.innerHTML = (keyCode - 97 + 1);
+		let number = focusedDiv.id;
+		let row = Math.floor(number / 9);
+		let col = number % 9;
+		// console.log(row, col);
+		board[row][col] = (keyCode - 97 + 1)
+		cnt++;
+	}
+	else if(keyCode == 8 || keyCode == 37 || keyCode == 39){
+		board[row][col] = 0;
+		const focusedDiv = document.activeElement;
+		focusedDiv.innerHTML = '';
+	}
+	// Prevent the default behavior of the key (e.g., typing the key in the div)
+	event.preventDefault();
+}
+
+GetPuzzle2.onclick = function () {
+	UnFillBoard();
+	cnt = 0;
+	// const editableDivs = document.querySelectorAll(".clickable");
+	editableDivs.forEach((element) => {
+		element.setAttribute("contentEditable", "true");
+	});
+}
+// this a will check for infinite loop because for wrong input code will go in infinite loop
+// so we have to stop this. this a will count 
+let a = 0;
 SolvePuzzle.onclick = () => {
-	
-	sudukoSolver(board, 9);
-	FillBoard(board);
+	if(cnt < 17){
+		// UnFillBoard();
+		alert("Wrong Input! Fill atleast 17 Cells.")
+		cnt = 0;
+	}
+	else{
+		a = 0;
+		sudukoSolver(board, 9);
+		FillBoard(board);
+	}
 };
 
 function sudukoSolver(board, n) {
 
-	solve(board, n)
-
-
+	let ans = solve(board, n , 0)
+	if(ans == false)
+	{
+		UnFillBoard();
+		a = 0;
+		alert("Wrong input typed! Fill again.");
+	}
 }
 function isvalid(board, row, col, num) {
 	for (let i = 0; i < 9; i++) {
@@ -62,7 +145,11 @@ function isvalid(board, row, col, num) {
 	}
 	return true;
 }
-function solve(board, n) {
+
+function solve(board, n , fcnt) {
+	a++;
+	// console.log(a);
+	if(a >= 1000000) return false; //for infinite loop
 	for (let i = 0; i < n; i++) {
 		for (let j = 0; j < n; j++) {
 			if (board[i][j] === 0) {
@@ -70,7 +157,7 @@ function solve(board, n) {
 					if (isvalid(board, i, j, num) == true) {
 						board[i][j] = num;
 
-						if (solve(board, n) == true) {
+						if (solve(board, n , fcnt+1) == true) {
 							return true;
 						}
 
